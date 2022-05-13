56dc97e3bc3bd2c14fd214a338d0ba86e6c2de10
otavioalbucosta <otavio-albucosta@hotmail.com>
2022-05-13 08:42:56 -0300

    Initial Commit

---
+++ CBLManager/CBLManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CBLManagerApp.swift
+//  CBLManager
+//
+//  Created by Otávio Albuquerque on 13/05/22.
+//
+
+import SwiftUI
+
+@main
+struct CBLManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
